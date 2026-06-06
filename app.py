e1a6905f19795cd5a535fb2c9da45a99af9065e8
j <shriyal.jainam@gmail.com>
2026-06-06 11:09:05 +0530

    Updated message1

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return {
-        "message": "New Deployment Successful!!"
+        "message": "Hello shriyahh!!"
     }
 
 if __name__ == "__main__":
